71b5d7885868ed39bedbeb699dd4067c8a7a3604
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2025-09-28 22:15:55 +0200

    fix: update apiBase URL for production environment settings

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Production environment settings
 export const environment = {
   production: true,
-  apiBase: 'https://fullstack-app-prod.azurewebsites.net/',
+  apiBase: 'https://fullstack-app-prod.azurewebsites.net/weatherforecast',
 } as const;
