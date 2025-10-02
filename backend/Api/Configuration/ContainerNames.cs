009ecfd03f7b0d79bc35d424f4f8211b7f719653
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2025-10-02 21:34:05 +0200

    feat: add Cosmos DB and key Vault configuration and health check controller

---
+++ backend/Api/Configuration/ContainerNames.cs
@@ -0,0 +1,9 @@
+namespace Api.Configuration;
+
+public class ContainerNames
+{
+    public string Products { get; set; } = string.Empty;
+    public string Carts { get; set; } = string.Empty;
+    public string Orders { get; set; } = string.Empty;
+    public string Categories { get; set; } = string.Empty;
+}
