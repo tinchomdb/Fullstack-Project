142980a0472d15cd3ab9a49ab9fde14e95c1685b
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2025-10-12 19:23:54 +0200

    Implement caching for categories, products, and carousel slides

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure services
 builder.AddKeyVaultConfiguration();
 builder.Services.AddCosmosDb(builder.Configuration);
+builder.Services.AddCaching(builder.Configuration);
+builder.Services.DecorateRepositoriesWithCaching();
 builder.Services.AddAuthenticationAndAuthorization(builder.Configuration);
 builder.Services.AddCorsPolicy(builder.Configuration);
 builder.Services.AddSwagger();
